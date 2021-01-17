00d94259b57bf376796a068a9d19d981df0c81d1
Xyresic <ericjmlam@gmail.com>
2021-01-17 16:20:28 -0500

    completed doppler effect

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,35 @@ let logistic_scale = (num) => {
 let beta = logistic_scale(speed_slider.value / 10);
 let gamma = 1 / Math.sqrt(1 - beta ** 2);
 let theta = parseInt(angle_slider.value);
+let hex = HSVtoRGB(0, 1, 1);
 
 let start;
 $(document).ready(() => {
     start = Date.now();
 });
+
+//source: https://stackoverflow.com/a/32471000
+function HSVtoRGB(h, s, v) {
+    let r, g, b, i, f, p, q, t;
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
 
 let update_gamma = function(event) {
     beta = logistic_scale(this.value / 10);
@@ -36,7 +60,29 @@ let update_theta = function() {
 }
 
 let update_color = function() {
-
+    let freq = 400 + parseInt(this.value);
+    hex = HSVtoRGB((freq - 400) * 0.8 / 390, 1, 1);
+    $('.wave:odd').attr('stroke', `rgba(${hex.r}, ${hex.g}, ${hex.b}, 1)`);
+    let freq_prime;
+    if ($('.reverse')[0].checked) {
+        freq_prime = freq * Math.sqrt((1 + beta) / (1 - beta));
+    } else {
+        freq_prime = freq * Math.sqrt((1 - beta) / (1 + beta));
+    }
+    if (freq_prime < 400) {
+        $('.wave:even').attr('stroke', 'transparent');
+        $('.infra').show();
+        $('.ultra').hide();
+    } else if (freq_prime > 790) {
+        $('.wave:even').attr('stroke', 'transparent');
+        $('.infra').hide();
+        $('.ultra').show();
+    } else {
+        $('.infra').hide();
+        $('.ultra').hide();
+        let hex_prime = HSVtoRGB((freq_prime - 400) * 0.8 / 390, 1, 1);
+        $('.wave:even').attr('stroke', `rgba(${hex_prime.r}, ${hex_prime.g}, ${hex_prime.b}, 1)`);
+    }
 }
 
 speed_slider.addEventListener('input', update_gamma);
