b462ec6b37b369b9870918c992d810ace466b4bf
Xyresic <ericjmlam@gmail.com>
2021-01-17 14:35:40 -0500

    completed aberration

--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,8 @@ let color_slider = $('#hex')[0];
 let logistic_scale = (num) => {
     return 2 / (1 + Math.exp(-num)) - 1;
 }
-let mult = logistic_scale(speed_slider.value / 10);
-let gamma = 1 / Math.sqrt(1 - mult);
+let beta = logistic_scale(speed_slider.value / 10);
+let gamma = 1 / Math.sqrt(1 - beta ** 2);
 let theta = parseInt(angle_slider.value);
 
 let start;
@@ -15,9 +15,9 @@ $(document).ready(() => {
 });
 
 let update_gamma = function(event) {
-    mult = logistic_scale(this.value / 10);
-    gamma = 1 / Math.sqrt(1 - mult);
-    $('#speed-val').text(mult.toFixed(5) + 'c');
+    beta = logistic_scale(this.value / 10);
+    gamma = 1 / Math.sqrt(1 - beta);
+    $('#speed-val').text(beta.toFixed(5) + 'c');
     $('.ruler:odd').width(`${40 / gamma}vw`);
     let hands = $('.hand');
     hands.css('animation', 'none');
@@ -30,6 +30,9 @@ let update_gamma = function(event) {
 let update_theta = function() {
     theta = parseInt(this.value)
     $('#theta-val').text(theta + '°');
+    $('svg:odd').css('transform', `translate(-50%, 0) rotate(${-theta}deg)`);
+    let theta_prime = Math.atan2(Math.sin(theta * Math.PI / 180), gamma * (Math.cos(theta * Math.PI / 180) + beta)) * 180 / Math.PI;
+    $('svg:even').css('transform', `translate(-50%, 0) rotate(${-theta_prime}deg)`);
 }
 
 let update_color = function() {
